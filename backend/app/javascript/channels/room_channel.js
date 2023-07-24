7b5e7a0e9be0139b0cb5ccc54af14bae81eb369c
gon0821 <gonty821@gmail.com>
2023-07-25 01:01:17 +0900

    Implement basic chat functionality

---
+++ backend/app/javascript/channels/room_channel.js
@@ -0,0 +1,27 @@
+import consumer from "channels/consumer"
+
+const appRoom = consumer.subscriptions.create("RoomChannel", {
+  connected() {
+
+  },
+
+  disconnected() {
+
+  },
+
+  received(data) {
+    const messages = document.getElementById('messages');
+    messages.insertAdjacentHTML('beforeend', data['message']);
+  },
+
+  speak: function(message) {
+    return this.perform('speak', {message: message});
+  }
+});
+
+document.getElementById('new_message').addEventListener('submit', function(event) {
+  let messageContent = document.getElementById('message_content');
+    appRoom.speak(messageContent.value);
+    messageContent.value = '';
+    event.preventDefault();
+})
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,15 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_21_053315) do
-  create_table "patients", charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_092425) do
+  create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "secret_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["secret_id"], name: "index_patients_on_secret_id", unique: true
   end
 
-  create_table "users", charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
