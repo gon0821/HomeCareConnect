60b74f5febf3234896ef81602de8394fc24b74f2
gon0821 <gonty821@gmail.com>
2023-07-28 02:42:41 +0900

    Enable Date Selection for Schedule

--- backend/app/controllers/schedules_controller.rb
+++ backend/app/controllers/schedules_controller.rb
@@ -6,22 +6,25 @@ class SchedulesController < ApplicationController
 
   # 今日の服用スケジュールを取得し、一覧表示する
   def index
-    schedules_today = Schedule.joins(:medication).where(medications: { secret_patient_id: @patient.secret_id}, date: Date.today)
-    schedules_sorted_by_time = schedules_today.sort_by do |schedule|
+    date = params[:date]&.to_date || Date.today
+    schedules_on_date = Schedule.joins(:medication).where(medications: { secret_patient_id: @patient.secret_id}, date: date)
+    schedules_sorted_by_time = schedules_on_date.sort_by do |schedule|
       schedule.medication.send("time#{schedule.time_slot}").seconds_since_midnight
     end
     @schedules = schedules_sorted_by_time
+    @date = date
   end
 
   # 服用スケジュールの確認状況を更新する
   def update_confirmation
+    date = params[:date]&.to_date || Date.today
     checked_schedules = Schedule.joins(:medication).where(medications: { secret_patient_id: @patient.secret_id }, id: params[:schedules])
-    unchecked_schedules = Schedule.joins(:medication).where(medications: { secret_patient_id: @patient.secret_id }, date: Date.today).where.not(id: params[:schedules])
+    unchecked_schedules = Schedule.joins(:medication).where(medications: { secret_patient_id: @patient.secret_id }, date: date).where.not(id: params[:schedules])
 
     checked_schedules.update_all(confirmation: true)
     unchecked_schedules.update_all(confirmation: false)
 
-    redirect_to patient_schedules_path(current_patient)
+    redirect_to patient_schedules_path(@patient, date: date)
   end
 
   private
