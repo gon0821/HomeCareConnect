d2273b4143a46f0a6e76715abccee965ced7ef18
gon0821 <gonty821@gmail.com>
2023-07-29 23:05:58 +0900

    Resolve chat reload error

--- backend/app/javascript/channels/room_channel.js
+++ backend/app/javascript/channels/room_channel.js
@@ -1,28 +1,47 @@
 import consumer from "channels/consumer"
 
-let secretId = document.getElementById('messages').dataset.secretId;
+document.addEventListener("turbo:load", function(){
+  if (document.getElementById('messages') != null) {
+    if (document.getElementById('messages').dataset != null) {
+      if (document.getElementById('messages').dataset.secretId != null) {
 
-const appRoom = consumer.subscriptions.create({channel: "RoomChannel", secret_id: secretId}, {
-  connected() {
-    console.log("Connected to the channel with secretId:", secretId);
-  },
+        let secretId = document.getElementById('messages').dataset.secretId;
 
-  disconnected() {
-  },
+        const appRoom = consumer.subscriptions.create({channel: "RoomChannel", secret_id: secretId}, {
+          connected() {
+            console.log("Connected to the channel with secretId:", secretId);
+          },
 
-  received(data) {
-    const messages = document.getElementById('messages');
-    messages.insertAdjacentHTML('beforeend', data['message']);
-  },
+          disconnected() {
+          },
 
-  speak: function(message) {
-    return this.perform('speak', { message: message, secret_id: secretId});
+          received(data) {
+            const messages = document.getElementById('messages');
+            messages.insertAdjacentHTML('beforeend', data['message']);
+          },
+
+          speak: function(message) {
+            return this.perform('speak', { message: message, secret_id: secretId});
+          }
+        });
+
+        document.getElementById('new_message').addEventListener('submit', function(event) {
+          let messageContent = document.getElementById('message_content');
+            appRoom.speak(messageContent.value);
+            messageContent.value = '';
+            event.preventDefault();
+        });
+
+      }
+    }
   }
+
 });
 
-document.getElementById('new_message').addEventListener('submit', function(event) {
-  let messageContent = document.getElementById('message_content');
-    appRoom.speak(messageContent.value);
-    messageContent.value = '';
-    event.preventDefault();
-})
+document.addEventListener("turbo:before-fetch-request", function () {
+  var subscriptions = consumer.subscriptions['subscriptions'];
+
+  subscriptions.forEach(function (subscription) {
+    consumer.subscriptions.remove(subscription);
+  });
+});
