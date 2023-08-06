ab16f10159b7ab4210a95d177c6dcaab724f42bd
gon0821 <gonty821@gmail.com>
2023-08-06 21:58:03 +0900

    Delete consolelog

--- backend/app/javascript/flash_message.js
+++ backend/app/javascript/flash_message.js
@@ -2,7 +2,6 @@
   document.addEventListener('turbo:load', (event) => {
     setTimeout(function () {
       let flashes = document.getElementsByClassName('flash');
-      console.log(flashes);
       for (let i = 0; i < flashes.length; i++) {
         let flash = flashes[i];
         flash.style.opacity = 0;
