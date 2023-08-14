2eaca1ea4eb8be08596267dd12435f43db1b8ef3
gon0821 <gonty821@gmail.com>
2023-08-15 03:09:56 +0900

    Redirect chat room from top-page if logged

--- backend/app/controllers/home_controller.rb
+++ backend/app/controllers/home_controller.rb
@@ -1,10 +1,18 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: [:show]
+  before_action :redirect_if_login, only: [:index]
 
   def index
   end
 
   def show
     @user = current_user
+  end
+
+  private
+  def redirect_if_login
+    if user_signed_in?
+      redirect_to rooms_show_path
+    end
   end
 end
